4ddac8e99572961007624e0f6f2042fc13d8b416
Mark Cottman-Fields <mark.cottmanfields@qcif.edu.au>
2025-03-04 09:18:31 +1000

    update latest release text

--- .support/generate_latest_release_info.sh
+++ .support/generate_latest_release_info.sh
@@ -13,8 +13,11 @@ curl --silent -L -o "${DATA_FILE}" \
 VERSION=$(jq -r '.tag_name' "${DATA_FILE}")
 URL=$(jq -r '.html_url' "${DATA_FILE}")
 PUB_DATE=$(jq -r '.published_at | fromdateiso8601 | strftime("%a, %d %B %Y")' "${DATA_FILE}")
+MSG="The <a href='${URL}' target='_blank'>most recent ReDBox version is <strong>${VERSION}</strong></a> released on ${PUB_DATE}."
 
 # e.g. The [most recent ReDBox version is <tag_name>](<html_url>) released on [<published_at>].
 # e.g. The [most recent ReDBox version is *v3.6*](https://github.com/redbox-mint/redbox-portal/releases/tag/v3.6) released on 01 May 2024.
 
-echo "The <a href='${URL}' target='_blank'>most recent ReDBox version is <strong>${VERSION}</strong></a> released on ${PUB_DATE}." > "${HTML_FILE}"
+echo "Generated text: ${MSG}"
+
+echo "${MSG}" > "${HTML_FILE}"
